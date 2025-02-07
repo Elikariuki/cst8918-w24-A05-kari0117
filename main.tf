0e367c79c43f4140b25e15e0c81db0247b3d0118
Elikariuki <ngugidoc4000@gmail.com>
2025-02-07 16:02:35 -0500

    Defining the subnet

--- main.tf
+++ main.tf
@@ -47,3 +47,10 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Define the subnet
+resource "azurerm_subnet" "webserver" {
+  name                 = "${var.labelPrefix}A05Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
